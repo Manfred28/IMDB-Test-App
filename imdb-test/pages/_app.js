be0b7dcd46596c8363f6ccc474e6d12e8be66312
István Kozma <pisti_kozma@hotmail.com>
2020-10-04 15:35:48 +0200

    create theme and setup mui with nextjs

--- imdb-test/pages/_app.js
+++ imdb-test/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../styles/theme';
+import 'tachyons/css/tachyons.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Head>
+          <title>IMDB Test App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
