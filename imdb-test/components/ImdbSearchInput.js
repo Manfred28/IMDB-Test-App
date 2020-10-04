ea9d17129570e04ade2cde244cfbf4ab92abcd4b
István Kozma <pisti_kozma@hotmail.com>
2020-10-04 18:49:38 +0200

    add spinner to input

--- imdb-test/components/ImdbSearchInput.js
+++ imdb-test/components/ImdbSearchInput.js
@@ -1,32 +1,39 @@
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { IMDB_API_KEY } from '../constants/imdb';
 import debounce from 'lodash/debounce';
 import { SearchContext } from '../providers/SearchProvider';
 
 function ImdbSearchInput() {
+  const [isLoading, setIsloading] = useState(false);
   const search = useContext(SearchContext);
 
   const onChange = useCallback(
     debounce((text) => {
       if (!text) return;
 
+      setIsloading(true);
       fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=${IMDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${text}`
       )
         .then((response) => response.json())
-        .then((data) => search.setResults(data.results));
+        .then((data) => search.setResults(data.results))
+        .then(() => setIsloading(false));
     }, 500),
     []
   );
 
   return (
-    <TextField
-      variant="outlined"
-      className="w-100"
-      placeholder="Enter a movie title"
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div>
+      <TextField
+        variant="outlined"
+        className="w-100"
+        placeholder="Enter a movie title"
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {isLoading && <CircularProgress />}
+    </div>
   );
 }
 
