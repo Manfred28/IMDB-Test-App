b035b45bd166de1a129a350d154f079c6676a011
István Kozma <pisti_kozma@hotmail.com>
2020-10-04 18:07:26 +0200

    display wiki snippet when title is clicked

--- imdb-test/components/Movie.js
+++ imdb-test/components/Movie.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import classnames from 'classnames';
+import { Markup } from 'interweave';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -8,15 +10,37 @@ const useStyles = makeStyles((theme) => ({
 }));
 function Movie({ movie }) {
   const classes = useStyles();
+  const [wikiEntry, setWikiEntry] = useState(null);
+
+  const wikiSearch = () => {
+    if (wikiEntry) return;
+
+    fetch(
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=${encodeURIComponent(
+        movie.title
+      )}`
+    )
+      .then((response) => response.json())
+      .then((data) => setWikiEntry(data.query.search[0]));
+  };
 
   return (
     <div className={classnames('ba pa2 mb2 br2', classes.root)}>
-      <span className="mr1 b">Title:</span>
-      <span className="mr2">{movie.title}</span>
-      <span className="mr1 b">Popularity:</span>
-      <span className="mr2">{movie.popularity}</span>
-      <span className="mr1 b">Original Language:</span>
-      <span>{movie.original_language}</span>
+      <div>
+        <span className="mr1 b">Title:</span>
+        <span className="mr2 pointer" onClick={wikiSearch}>
+          {movie.title}
+        </span>
+        <span className="mr1 b">Popularity:</span>
+        <span className="mr2">{movie.popularity}</span>
+        <span className="mr1 b">Original Language:</span>
+        <span>{movie.original_language}</span>
+      </div>
+      {wikiEntry && (
+        <div className="mt2">
+          <Markup content={wikiEntry.snippet} containerTagName="div" />
+        </div>
+      )}
     </div>
   );
 }
