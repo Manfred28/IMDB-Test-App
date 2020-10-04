187c324bc18306417a18d208d5053b7ebbb751ac
István Kozma <pisti_kozma@hotmail.com>
2020-10-04 17:10:30 +0200

    store search results in context

--- imdb-test/components/ImdbSearchInput.js
+++ imdb-test/components/ImdbSearchInput.js
@@ -1,9 +1,13 @@
+import { useCallback, useContext } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { IMDB_API_KEY } from '../constants/imdb';
 import debounce from 'lodash/debounce';
+import { SearchContext } from '../providers/SearchProvider';
 
 function ImdbSearchInput() {
-  const onChange = React.useCallback(
+  const search = useContext(SearchContext);
+
+  const onChange = useCallback(
     debounce((text) => {
       if (!text) return;
 
@@ -11,7 +15,7 @@ function ImdbSearchInput() {
         `https://api.themoviedb.org/3/search/movie?api_key=${IMDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${text}`
       )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => search.setResults(data.results));
     }, 500),
     []
   );
